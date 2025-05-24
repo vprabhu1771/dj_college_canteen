167cd585bbb48e990a324155db8d447a95f4aee5
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-24 23:15:33 +0530

    model fix

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
@@ -42,13 +44,27 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.MALE)
     image = GenderedImageField(upload_to='profile/', blank=True)
+    phone = models.CharField(max_length=10, unique=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['gender']
+    REQUIRED_FIELDS = ['gender', 'phone',]
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
+
+class CustomerUser(CustomUser):
+    class Meta:
+        proxy = True
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
+
+
+class AdminUser(CustomUser):
+    class Meta:
+        proxy = True
+        verbose_name = 'Admin'
+        verbose_name_plural = 'Admins'
 
 class Category(models.Model):
     id=models.BigAutoField(primary_key=True)
@@ -97,7 +113,7 @@ class Product(models.Model):
 class Cart(models.Model):
     id = models.BigAutoField(primary_key=True)
     custom_user = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,blank=True,null=True)
-    Product =  models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
+    product =  models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
     qty = models.IntegerField()
 
     def __str__(self):
@@ -106,30 +122,32 @@ class Cart(models.Model):
     def total_price(self):
         return self.qty * self.product.price if self.product else 0
 
-    def grand_total(self):
-        cart_items = Cart.objects.filter(custom_user=self.custom_user)
-        total = sum (element.total_price()for element in cart_items)
+    @classmethod
+    def grand_total(cls, customer_id):
+        cart_items = cls.objects.filter(custom_user_id=customer_id)
+        total = sum(item.total_price() for item in cart_items)
+        return total
 
     class Meta:
         db_table = 'cart'
 
+# Order
 class OrderStatus(models.TextChoices):
     PENDING = 'PENDING',_('Pending')
     APPROVED = 'APPROVED',_('Approved')
     REJECTED = 'REJECTED',_('Rejected')
 
-class PaymentMethod(models.TextChoices):
+class PaymentMethodStatus(models.TextChoices):
     CASH = 'CASH',_('CASH')
     UPI = 'UPI',_('UPI')
     CARD = 'CARD',_('CARD')
 
-
 class Order(models.Model):
-    id=models.BigAutoField(primary_key=True)
-    custom_user = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,blank=True,null=True)
-    order_number = models.CharField(max_length=255,blank=True,null=True)
+    id = models.BigAutoField(primary_key=True)
+    customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+    order_number = models.CharField(max_length=20, unique=True, blank=True)
     order_date = models.DateTimeField(auto_now_add=True)
-    total_amount = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     order_status = models.CharField(
         max_length=255,
         choices=OrderStatus.choices,
@@ -137,37 +155,48 @@ class Order(models.Model):
     )
     payment_method = models.CharField(
         max_length=255,
-        choices=PaymentMethod.choices,
-        default=PaymentMethod.CASH
+        choices=PaymentMethodStatus.choices,
+        default=PaymentMethodStatus.CASH
     )
+
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
+
+    def generate_order_number(self):
+        today = datetime.date.today().strftime('%Y-%m-%d')
+        last_order = Order.objects.filter(order_date__date=today).order_by('-id').first()
+        if last_order:
+            last_number = int(last_order.order_number.split()[-1])
+            next_number = str(last_number + 1).zfill(3)
+        else:
+            next_number = '001'
+        return f"{today} {next_number}"
+
+    def __str__(self):
+        return f"{self.order_date.strftime('%d-%m-%Y %H:%M:%S')} {self.customer} {self.total_amount}"
 
     class Meta:
         db_table = 'order'
 
+# OrderItem
 class OrderItem(models.Model):
     id = models.BigAutoField(primary_key=True)
-    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
-    product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
-    qty = models.IntegerField(null=True,blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    discount = models.IntegerField(blank=True,null=True,default=0)
+    # order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    qty = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField()
 
-    def calculate_total_amount(self):
-        if self.price and self.qty:
-            subtotal = self.price * self.qty
-            if self.discount > 0:
-                discount_amount = (subtotal * self.discount) / 100
-                total_amount = subtotal - discount_amount
-            else:
-                total_amount = subtotal
-            return total_amount
-        return 0
+    # item_total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        # Automatically update amount based on the calculated total amount
-        self.amount = self.calculate_total_amount()
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return f"{self.order} {self.product.name} {self.qty} {self.unit_price} {self.amount}"
 
     class Meta:
         db_table = 'order_items'
