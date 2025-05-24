a29bfe7d836795147fc7181d98109ef9fc6602a6
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-24 22:31:31 +0530

    CategoryAdmin, BrandAdmin, ProductAdmin, CartAdmin, OrderAdmin added

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from backend.forms import CustomUserCreationForm, CustomUserChangeForm
-from backend.models import CustomUser
+from backend.models import CustomUser, Category, Brand, Product, Cart, OrderItem, Order
 from django.utils.html import format_html
 
 
@@ -39,3 +39,46 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_tag',)
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="150" height="150" />'.format(obj.image_path.url))
+
+    image_tag.short_description = 'Image'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category', 'brand', 'price', 'qty', 'name', 'image_tag',)
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="150" height="150" />'.format(obj.image_path.url))
+
+    image_tag.short_description = 'Image'
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'custom_user', 'Product', 'qty',)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'custom_user', 'order_number', 'order_date', 'total_amount', 'order_status', 'payment_method',
+    )
+    inlines = [OrderItemInline]
